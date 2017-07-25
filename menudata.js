872378b968ec3c6aa4788bc5ff8832a5fbd1af01
William Dawson <dawson@fairport.aics27.riken.jp>
2017-07-25 12:58:17 +0900

    Daily build: 2017-07-25-12:58:17

--- menudata.js
+++ menudata.js
@@ -104,7 +104,6 @@ var menudata={children:[
 {text:"i",url:"functions_vars.html#index_i"},
 {text:"l",url:"functions_vars.html#index_l"},
 {text:"m",url:"functions_vars.html#index_m"},
-{text:"n",url:"functions_vars.html#index_n"},
 {text:"o",url:"functions_vars.html#index_o"},
 {text:"p",url:"functions_vars.html#index_p"},
 {text:"r",url:"functions_vars.html#index_r"},
