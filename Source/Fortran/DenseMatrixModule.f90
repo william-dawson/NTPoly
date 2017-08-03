a4354b9e8cac76a6f0b72630eead750736678aa3
William Dawson <william.dawson@riken.jp>
2017-08-03 13:04:43 +0900

    Separated the linear algebra routines out of the sparse matrix module.

--- Source/Fortran/DenseMatrixModule.f90
+++ Source/Fortran/DenseMatrixModule.f90
@@ -6,10 +6,12 @@
 MODULE DenseMatrixModule
   USE DataTypesModule
   USE SparseMatrixModule
+  USE TripletListModule
   USE TripletModule
   IMPLICIT NONE
   PRIVATE
   PUBLIC :: ConstructDenseFromSparse
+  PUBLIC :: ConstructSparseFromDense
   PUBLIC :: MultiplyDense
 CONTAINS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   !> A function that converts a sparse matrix to a dense matrix.
@@ -17,8 +19,8 @@ CONTAINS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   !! @param[inout] dense_matrix a preallocated
   PURE SUBROUTINE ConstructDenseFromSparse(sparse_matrix,dense_matrix)
     !! Parameters
-    TYPE(SparseMatrix_t), INTENT(in) :: sparse_matrix
-    REAL(NTREAL), DIMENSION(:,:), INTENT(inout) :: dense_matrix
+    TYPE(SparseMatrix_t), INTENT(IN) :: sparse_matrix
+    REAL(NTREAL), DIMENSION(:,:), INTENT(INOUT) :: dense_matrix
     !! Helper Variables
     INTEGER :: inner_counter, outer_counter
     INTEGER :: elements_per_inner
@@ -42,15 +44,44 @@ CONTAINS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     END DO
   END SUBROUTINE ConstructDenseFromSparse
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !> A function that converts a dense matrix to a sparse matrix.
+  !! @param[in] dense_matrix to convert.
+  !! @param[out] sparse_matrix to build.
+  !! @param[in] threshold value for pruning.
+  PURE SUBROUTINE ConstructSparseFromDense(dense_matrix,sparse_matrix,threshold)
+    !! Parameters
+    REAL(NTREAL), DIMENSION(:,:), INTENT(IN) :: dense_matrix
+    TYPE(SparseMatrix_t), INTENT(OUT) :: sparse_matrix
+    REAL(NTREAL), INTENT(IN) :: threshold
+    !! Local Variables
+    INTEGER :: inner_counter, outer_counter
+    TYPE(Triplet_t) :: temporary
+    TYPE(TripletList_t) :: temporary_list
+
+    CALL ConstructEmptySparseMatrix(sparse_matrix, &
+         & SIZE(dense_matrix,DIM=1), SIZE(dense_matrix,DIM=2))
+
+    DO outer_counter = 1, sparse_matrix%columns
+       temporary%index_column = outer_counter
+       DO inner_counter = 1, sparse_matrix%rows
+          temporary%point_value = dense_matrix(outer_counter,inner_counter)
+          IF (ABS(temporary%point_value) .GT. threshold) THEN
+             temporary%index_row = inner_counter
+             CALL AppendToTripletList(temporary_list,temporary)
+          END IF
+       END DO
+    END DO
+  END SUBROUTINE ConstructSparseFromDense
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   !> A wrapper for multiplying two dense matrices.
   !! @param[in] MatA the first matrix.
   !! @param[in] MatB the second matrix.
-  !! @param[inout] MatC = MatA*MatB
+  !! @param[inout] MatC = MatA*MatB. Preallocated.
   PURE SUBROUTINE MultiplyDense(MatA,MatB,MatC)
     !! Parameters
-    REAL(NTREAL), DIMENSION(:,:), INTENT(in) :: MatA
-    REAL(NTREAL), DIMENSION(:,:), INTENT(in) :: MatB
-    REAL(NTREAL), DIMENSION(:,:), INTENT(inout) :: MatC
+    REAL(NTREAL), DIMENSION(:,:), INTENT(IN) :: MatA
+    REAL(NTREAL), DIMENSION(:,:), INTENT(IN) :: MatB
+    REAL(NTREAL), DIMENSION(:,:), INTENT(INOUT) :: MatC
 
     MatC = MATMUL(MatA,MatB)
   END SUBROUTINE MultiplyDense
--- Source/Wrapper/SparseMatrixModule_wrp.f90
+++ Source/Wrapper/SparseMatrixModule_wrp.f90
@@ -6,10 +6,8 @@ MODULE SparseMatrixModule_wrp
   USE SparseMatrixModule, ONLY : SparseMatrix_t, &
        & ConstructEmptySparseMatrix, ConstructSparseMatrixFromFile, &
        & ConstructFromTripletList, DestructSparseMatrix, CopySparseMatrix, &
-       & ScaleSparseMatrix, IncrementSparseMatrix, Gemm, &
        & TransposeSparseMatrix, PrintSparseMatrix, MatrixToTripletList, &
-       & GetRows, GetColumns, ComposeSparseMatrixColumns, DotSparseMatrix, &
-       & PairwiseMultiplySparseMatrix
+       & GetRows, GetColumns, ComposeSparseMatrixColumns
   USE TripletListModule_wrp, ONLY : TripletList_wrp
   USE WrapperModule, ONLY : SIZE_wrp
   USE ISO_C_BINDING, ONLY : c_int, c_char, c_bool
@@ -29,11 +27,6 @@ MODULE SparseMatrixModule_wrp
   PUBLIC :: GetRows_wrp
   PUBLIC :: GetColumns_wrp
   PUBLIC :: CopySparseMatrix_wrp
-  PUBLIC :: ScaleSparseMatrix_wrp
-  PUBLIC :: IncrementSparseMatrix_wrp
-  PUBLIC :: PairwiseMultiplySparseMatrix_wrp
-  PUBLIC :: DotSparseMatrix_wrp
-  PUBLIC :: Gemm_wrp
   PUBLIC :: TransposeSparseMatrix_wrp
   PUBLIC :: PrintSparseMatrix_wrp
   PUBLIC :: PrintSparseMatrixF_wrp
@@ -153,112 +146,6 @@ CONTAINS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     h_this = TRANSFER(ih_this,h_this)
     columns = GetColumns(h_this%data)
   END SUBROUTINE GetColumns_wrp
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !> Wrap the scale a sparse matrix by a constant routine.
-  !! @param[inout] ih_this handle to the matrix to scale.
-  !! @param[in] constant scale factor.
-  PURE SUBROUTINE ScaleSparseMatrix_wrp(ih_this, constant) &
-       & bind(c,name="ScaleSparseMatrix_wrp")
-    INTEGER(kind=c_int), INTENT(inout) :: ih_this(SIZE_wrp)
-    REAL(NTREAL), INTENT(in) :: constant
-    TYPE(SparseMatrix_wrp) :: h_this
-
-    h_this = TRANSFER(ih_this,h_this)
-    CALL ScaleSparseMatrix(h_this%data,constant)
-  END SUBROUTINE ScaleSparseMatrix_wrp
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !> Wrap matrix incrementing function.
-  !! @param[in] ih_matA handle to Matrix A.
-  !! @param[in,out] ih_matB handle to Matrix B.
-  !! @param[in] alpha_in multiplier.
-  !! @param[in] threshold_in for flushing values to zero.
-  PURE SUBROUTINE IncrementSparseMatrix_wrp(ih_matA, ih_matB, alpha_in, &
-       & threshold_in) bind(c,name="IncrementSparseMatrix_wrp")
-    INTEGER(kind=c_int), INTENT(in) :: ih_matA(SIZE_wrp)
-    INTEGER(kind=c_int), INTENT(inout) :: ih_matB(SIZE_wrp)
-    REAL(NTREAL), INTENT(in) :: alpha_in
-    REAL(NTREAL), INTENT(in) :: threshold_in
-    TYPE(SparseMatrix_wrp) :: h_matA
-    TYPE(SparseMatrix_wrp) :: h_matB
-
-    h_matA = TRANSFER(ih_matA,h_matA)
-    h_matB = TRANSFER(ih_matB,h_matB)
-    CALL IncrementSparseMatrix(h_matA%data, h_matB%data, alpha_in, threshold_in)
-  END SUBROUTINE IncrementSparseMatrix_wrp
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !> Wrap matrix dot product function.
-  !! @param[in] ih_matA handle to Matrix A.
-  !! @param[in,out] ih_matB handle to Matrix B.
-  PURE FUNCTION DotSparseMatrix_wrp(ih_matA, ih_matB) RESULT(product) &
-       & bind(c,name="DotSparseMatrix_wrp")
-    INTEGER(kind=c_int), INTENT(in) :: ih_matA(SIZE_wrp)
-    INTEGER(kind=c_int), INTENT(in) :: ih_matB(SIZE_wrp)
-    REAL(NTREAL) :: product
-    TYPE(SparseMatrix_wrp) :: h_matA
-    TYPE(SparseMatrix_wrp) :: h_matB
-
-    h_matA = TRANSFER(ih_matA,h_matA)
-    h_matB = TRANSFER(ih_matB,h_matB)
-    product = DotSparseMatrix(h_matA%data, h_matB%data)
-  END FUNCTION DotSparseMatrix_wrp
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !> Wrap pairwise matrix multiplication function.
-  !! @param[in] ih_matA handle to Matrix A.
-  !! @param[in] ih_matB handle to Matrix B.
-  !! @param[out] ih_matC = A pairwise B
-  SUBROUTINE PairwiseMultiplySparseMatrix_wrp(ih_matA, ih_matB, ih_matC) &
-       & bind(c,name="PairwiseMultiplySparseMatrix_wrp")
-    INTEGER(kind=c_int), INTENT(in) :: ih_matA(SIZE_wrp)
-    INTEGER(kind=c_int), INTENT(in) :: ih_matB(SIZE_wrp)
-    INTEGER(kind=c_int), INTENT(inout) :: ih_matC(SIZE_wrp)
-    TYPE(SparseMatrix_wrp) :: h_matA
-    TYPE(SparseMatrix_wrp) :: h_matB
-    TYPE(SparseMatrix_wrp) :: h_matC
-
-    h_matA = TRANSFER(ih_matA,h_matA)
-    h_matB = TRANSFER(ih_matB,h_matB)
-    h_matC = TRANSFER(ih_matC,h_matC)
-
-    CALL PairwiseMultiplySparseMatrix(h_matA%data, h_matB%data, h_matC%data)
-  END SUBROUTINE PairwiseMultiplySparseMatrix_wrp
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !> Wrap matrix multiplication function.
-  !! @param[in] ih_matA handle to Matrix A.
-  !! @param[in] ih_matB handle to Matrix B.
-  !! @param[out] ih_matC = alpha*matA*op( matB ) + beta*matC.
-  !! @param[in] IsATransposed true if A is already transposed.
-  !! @param[in] IsBTransposed true if B is already transposed.
-  !! @param[in] alpha scales the multiplication.
-  !! @param[in] beta scales matrix we sum on to.
-  !! @param[in] threshold for flushing values to zero.
-  !! @param[inout] ih_blocked_memory_pool handle to memory pool.
-  SUBROUTINE Gemm_wrp(ih_matA, ih_matB, ih_matC, IsATransposed, &
-       & IsBTransposed, alpha, beta, threshold, ih_blocked_memory_pool) &
-       & bind(c,name="Gemm_wrp")
-    INTEGER(kind=c_int), INTENT(in) :: ih_matA(SIZE_wrp)
-    INTEGER(kind=c_int), INTENT(in) :: ih_matB(SIZE_wrp)
-    INTEGER(kind=c_int), INTENT(inout) :: ih_matC(SIZE_wrp)
-    LOGICAL(kind=c_bool), INTENT(in) :: IsATransposed
-    LOGICAL(kind=c_bool), INTENT(in) :: IsBTransposed
-    REAL(NTREAL), INTENT(in) :: alpha
-    REAL(NTREAL), INTENT(in) :: beta
-    REAL(NTREAL), INTENT(in) :: threshold
-    INTEGER(kind=c_int), INTENT(inout) :: ih_blocked_memory_pool(SIZE_wrp)
-    TYPE(SparseMatrix_wrp) :: h_matA
-    TYPE(SparseMatrix_wrp) :: h_matB
-    TYPE(SparseMatrix_wrp) :: h_matC
-    TYPE(MatrixMemoryPool_wrp) :: h_blocked_memory_pool
-
-    h_matA = TRANSFER(ih_matA,h_matA)
-    h_matB = TRANSFER(ih_matB,h_matB)
-    h_matC = TRANSFER(ih_matC,h_matC)
-    h_blocked_memory_pool = TRANSFER(ih_blocked_memory_pool, &
-         & h_blocked_memory_pool)
-
-    CALL Gemm(h_matA%data, h_matB%data, h_matC%data, &
-         & LOGICAL(IsATransposed), LOGICAL(IsBTransposed), alpha, &
-         & beta, threshold, h_blocked_memory_pool%data)
-  END SUBROUTINE Gemm_wrp
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   !> Wrap the matrix transpose function.
   !! @param[in] ih_matA handle to the matrix to be transposed.
