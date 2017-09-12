22bffa64f3411808148e90ac9f637d20906a574b
William Dawson <william.dawson@riken.jp>
2017-09-12 18:20:50 +0900

    Python dev path

--- Targets/Linux.cmake
+++ Targets/Linux.cmake
@@ -5,6 +5,6 @@ set(CMAKE_C_COMPILER mpicc)
 set(CMAKE_Fortran_COMPILER mpif90)
 set(CMAKE_CXX_COMPILER mpicxx)
 
-set(PYTHON_INCLUDE_PATH "/usr/include/python2.6/")
+#set(PYTHON_INCLUDE_PATH "/usr/include/python2.6/")
 set(CXX_TOOLCHAINFLAGS "-O3 -openmp -lgomp")
 set(F_TOOLCHAINFLAGS "-O3 -cpp -openmp")
