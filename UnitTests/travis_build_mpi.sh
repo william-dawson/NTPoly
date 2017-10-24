c378029dc47f1102688870ca940207e286625be1
William Dawson <william.dawson@riken.jp>
2017-10-24 18:07:53 +0900

    Changed filename to make it clear that this is travis-ci only

--- UnitTests/travis_build_mpi.sh
+++ UnitTests/travis_build_mpi.sh
@@ -1,4 +1,8 @@
-# FOR TRAVIS-CI ONLY
+## @file travis_build_mpi.sh
+## @brief FOR TRAVIS-CI ONLY
+## This downloads and builds openmpi from the website. This is necessary
+## since Travis-CI version of linux is too old to have version 2+ of openmpi.
+## This script checks for a cached version before downloading.
 if [ -f "openmpi-2.1.1/README" ]; then
   echo "Using cached openmpi";
 else
