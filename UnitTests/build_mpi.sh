37e4d794935e20d6e9674c025ef3c4d0fb5799f8
William Dawson <william.dawson@riken.jp>
2017-10-24 17:37:30 +0900

    Try checking for file instead...

--- UnitTests/build_mpi.sh
+++ UnitTests/build_mpi.sh
@@ -1,5 +1,5 @@
 # FOR TRAVIS-CI ONLY
-if [ -d "openmpi-2.1.1" ]; then
+if [ -f "openmpi-2.1.1/README" ]; then
   echo "Using cached openmpi";
 else
   wget https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.1.tar.gz;
