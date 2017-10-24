d646f9b8b427ff60e7849e398ec5abc9c6b9f4f4
William Dawson <william.dawson@riken.jp>
2017-10-24 18:00:12 +0900

    Moved build and config to the script.

--- UnitTests/build_mpi.sh
+++ UnitTests/build_mpi.sh
@@ -4,4 +4,7 @@ if [ -f "openmpi-2.1.1/README" ]; then
 else
   wget https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.1.tar.gz;
   tar xvf openmpi-2.1.1.tar.gz >/dev/null;
+  cd openmpi-2.1.1;
+  ./configure >/dev/null 2>&1;
+  make >/dev/null 2>&1;
 fi
