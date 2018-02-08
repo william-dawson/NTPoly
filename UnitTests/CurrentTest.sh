7c0c1d0d7bd834424f20b614ecbfe9fa03a21866
William Dawson <william.dawson@riken.jp>
2018-02-08 21:00:24 +0900

    Trying to reduce test time...

--- UnitTests/CurrentTest.sh
+++ UnitTests/CurrentTest.sh
@@ -22,8 +22,3 @@ export PROCESS_COLUMNS="$1"
 export PROCESS_ROWS="$2"
 export PROCESS_SLICES="$3"
 export PROCESSES="$4"
-
-@MPIEXEC@ @MPIEXEC_NUMPROC_FLAG@ $PROCESSES @oversubscribe@ @PYTHON_EXECUTABLE@ \
--m unittest -v testSolvers.TestSolvers.test_polarfunction
-@MPIEXEC@ @MPIEXEC_NUMPROC_FLAG@ $PROCESSES @oversubscribe@ @PYTHON_EXECUTABLE@ \
--m unittest -v testSolvers.TestSolvers.test_signfunction
