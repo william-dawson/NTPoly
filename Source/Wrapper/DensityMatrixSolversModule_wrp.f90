3b518fab35e39d9a06b2a832ee562c3f72ec6cc8
William Dawson <william.dawson@riken.jp>
2018-02-19 18:38:04 +0900

    Restricted doxygen to not include unit tests, wrappers

--- Source/Wrapper/DensityMatrixSolversModule_wrp.f90
+++ Source/Wrapper/DensityMatrixSolversModule_wrp.f90
@@ -107,34 +107,4 @@ CONTAINS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     CALL HPCP(h_Hamiltonian%data, h_InverseSquareRoot%data, nel, &
          & h_Density%data, chemical_potential_out, h_solver_parameters%data)
   END SUBROUTINE HPCP_wrp
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !> Compute the density matrix from a Hamiltonian using the HPCP method.
-  !! @param[in]  ih_Hamiltonian the matrix to compute the corresponding density.
-  !! @param[in]  ih_InverseSquareRoot of the overlap matrix.
-  !! @param[in]  nel the number of electrons.
-  !! @param[out] ih_Density the density matrix computed by this routine.
-  !! @param[out] chemical_potential_out the chemical potential calculated.
-  !! @param[in]  ih_solver_parameters parameters for the solver
-  ! SUBROUTINE HPCPPlus_wrp(ih_Hamiltonian, ih_InverseSquareRoot, &
-  !      & nel, ih_Density, chemical_potential_out, ih_solver_parameters) &
-  !      & bind(c,name="HPCPPlus_wrp")
-  !   INTEGER(kind=c_int), INTENT(in) :: ih_Hamiltonian(SIZE_wrp)
-  !   INTEGER(kind=c_int), INTENT(in) :: ih_InverseSquareRoot(SIZE_wrp)
-  !   INTEGER(kind=c_int), INTENT(in) :: nel
-  !   INTEGER(kind=c_int), INTENT(inout) :: ih_Density(SIZE_wrp)
-  !   REAL(NTREAL), INTENT(out) :: chemical_potential_out
-  !   INTEGER(kind=c_int), INTENT(in) :: ih_solver_parameters(SIZE_wrp)
-  !   TYPE(DistributedSparseMatrix_wrp) :: h_Hamiltonian
-  !   TYPE(DistributedSparseMatrix_wrp) :: h_InverseSquareRoot
-  !   TYPE(DistributedSparseMatrix_wrp) :: h_Density
-  !   TYPE(IterativeSolverParameters_wrp) :: h_solver_parameters
-  !
-  !   h_Hamiltonian = TRANSFER(ih_Hamiltonian,h_Hamiltonian)
-  !   h_InverseSquareRoot = TRANSFER(ih_InverseSquareRoot,h_InverseSquareRoot)
-  !   h_Density = TRANSFER(ih_Density,h_Density)
-  !   h_solver_parameters = TRANSFER(ih_solver_parameters, h_solver_parameters)
-  !
-  !   CALL HPCPPlus(h_Hamiltonian%data, h_InverseSquareRoot%data, nel, &
-  !        & h_Density%data, chemical_potential_out, h_solver_parameters%data)
-  ! END SUBROUTINE HPCPPlus_wrp
 END MODULE DensityMatrixSolversModule_wrp
