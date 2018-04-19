0c704d3862077e04169f709bea41ba0b59eeebfc
William Dawson <william.dawson@riken.jp>
2018-04-19 22:46:12 +0900

    A few fixes for Linux, caches Swig

--- UnitTests/travis_before_install.sh
+++ UnitTests/travis_before_install.sh
@@ -22,22 +22,29 @@ if [[ "$TESTOS" == "OSX" ]]; then
   brew install open-mpi
   brew install doxygen
   brew install cmake
+  sudo pip3 install scipy --upgrade
+  sudo pip3 install numpy --upgrade
+  sudo pip3 install mpi4py --upgrade
 else
   sudo ldconfig
   sudo apt-get install python-dev
   sudo apt-get install doxygen
-  wget https://downloads.sourceforge.net/swig/swig-3.0.12.tar.gz
-  tar xvf swig-3.0.12.tar.gz >/dev/null
-  cd swig-3.0.12
-  ./configure >/dev/null 2>&1
-  make >/dev/null 2>&1
+  if [ -f "swig-3.0.12/README" ]; then
+    echo "Using cached swig";
+  else
+    wget https://downloads.sourceforge.net/swig/swig-3.0.12.tar.gz;
+    tar xvf swig-3.0.12.tar.gz >/dev/null;
+    cd swig-3.0.12;
+    ./configure >/dev/null 2>&1;
+    make >/dev/null 2>&1;
+  fi
   sudo make install >/dev/null 2>&1
   cd ../
+  sudo pip install scipy --upgrade
+  sudo pip install numpy --upgrade
+  sudo pip install mpi4py --upgrade
 fi
 
-sudo pip3 install scipy --upgrade
-sudo pip3 install numpy --upgrade
-sudo pip3 install mpi4py --upgrade
 test -n $CC  && unset CC
 test -n $FCC  && unset FCC
 test -n $CXX && unset CXX
