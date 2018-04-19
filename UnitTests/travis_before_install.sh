ef75f6c3a9780481f356f658f88a73e43b742e1f
William Dawson <william.dawson@riken.jp>
2018-04-19 22:16:43 +0900

    Cached mpi

--- UnitTests/travis_before_install.sh
+++ UnitTests/travis_before_install.sh
@@ -1,22 +1,22 @@
 
 if [[ "$TESTOS" == "LINUX" ]]; then
   sudo apt-get install gfortran
+  if [ -f "openmpi-3.0.1/README" ]; then
+    echo "Using cached openmpi";
+  else
+    wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.1.tar.gz;
+    tar xvf openmpi-3.0.1.tar.gz >/dev/null;
+    cd openmpi-3.0.1;
+    ./configure >/dev/null 2>&1;
+    make >/dev/null 2>&1;
+  fi
+  cd openmpi-3.0.1
+  sudo make install >/dev/null 2>&1
+  cd ../
 fi
 
-# if [ -f "openmpi-3.0.1/README" ]; then
-  # echo "Using cached openmpi";
-# else
-  wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.1.tar.gz;
-  tar xvf openmpi-3.0.1.tar.gz >/dev/null;
-  cd openmpi-3.0.1;
-  ./configure >/dev/null 2>&1;
-  make >/dev/null 2>&1;
-# fi
-cd openmpi-3.0.1
-sudo make install >/dev/null 2>&1
-cd ../
-
 if [[ "$TESTOS" == "OSX" ]]; then
+  brew install open-mpi
   python install python3
   brew install doxygen
   brew install cmake
