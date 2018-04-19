3ef36410baa20cf4cfed50f9ca46a237281c3c42
William Dawson <william.dawson@riken.jp>
2018-04-19 22:28:45 +0900

    Switched swig method for mac.

--- UnitTests/travis_before_install.sh
+++ UnitTests/travis_before_install.sh
@@ -24,15 +24,15 @@ else
   sudo ldconfig
   sudo apt-get install python-dev
   sudo apt-get install doxygen
+  wget https://downloads.sourceforge.net/swig/swig-3.0.12.tar.gz
+  tar xvf swig-3.0.12.tar.gz >/dev/null
+  cd swig-3.0.12
+  ./configure >/dev/null 2>&1
+  make >/dev/null 2>&1
+  sudo make install >/dev/null 2>&1
+  cd ../
 fi
 
-wget https://downloads.sourceforge.net/swig/swig-3.0.12.tar.gz
-tar xvf swig-3.0.12.tar.gz >/dev/null
-cd swig-3.0.12
-./configure >/dev/null 2>&1
-make >/dev/null 2>&1
-sudo make install >/dev/null 2>&1
-cd ../
 sudo pip install scipy --upgrade
 sudo pip install numpy --upgrade
 sudo pip install mpi4py --upgrade
