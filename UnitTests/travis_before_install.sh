2ed05ca769808b0253ef4697dd6a8a35d1c8dc6d
William Dawson <william.dawson@riken.jp>
2018-04-19 18:48:58 +0900

    Attempt to add CI for Mac as well

---
+++ UnitTests/travis_before_install.sh
@@ -0,0 +1,31 @@
+
+if [[ "$TRAVIS_OS_NAME" == "OSX" ]]; then
+  brew install gcc;
+else
+  sudo apt-get install gfortran;
+fi
+travis_wait bash UnitTests/travis_build_mpi.sh
+cd openmpi-3.0.1
+sudo make install >/dev/null 2>&1
+cd ../
+sudo ldconfig
+if [[ "$TRAVIS_OS_NAME" == "OSX" ]]; then
+  python install python3;
+  brew install doxygen;
+else
+  sudo apt-get install python-dev;
+  sudo apt-get install doxygen;
+fi
+wget https://downloads.sourceforge.net/swig/swig-3.0.12.tar.gz
+tar xvf swig-3.0.12.tar.gz >/dev/null
+cd swig-3.0.12
+./configure >/dev/null 2>&1
+make >/dev/null 2>&1
+sudo make install >/dev/null 2>&1
+cd ../
+sudo pip install scipy --upgrade
+sudo pip install numpy --upgrade
+sudo pip install mpi4py --upgrade
+test -n $CC  && unset CC
+test -n $FCC  && unset FCC
+test -n $CXX && unset CXX
