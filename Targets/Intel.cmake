4cbad85a7a4613276c6467c968e7ec745ce5c408
William Dawson <william.dawson@riken.jp>
2018-04-19 16:59:55 +0900

    Simplified target files for Python bindings.

--- Targets/Intel.cmake
+++ Targets/Intel.cmake
@@ -5,7 +5,6 @@ set(CMAKE_C_COMPILER mpiicc)
 set(CMAKE_Fortran_COMPILER mpiifort)
 set(CMAKE_CXX_COMPILER mpiicpc)
 
-set(PYTHON_INCLUDE_PATH "/usr/include/python2.6/")
 set(CXX_TOOLCHAINFLAGS "-O3 -qopenmp -lgomp -fPIC")
 set(F_TOOLCHAINFLAGS "-O3 -fpp -qopenmp -fPIC")
 # Debug suggestions
