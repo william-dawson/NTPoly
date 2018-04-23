aa2e02f5aede2a7a201267dee6d23a229233bd0f
William Dawson <william.dawson@riken.jp>
2018-04-23 14:25:10 +0900

    For testing.

--- UnitTests/testSolvers.py
+++ UnitTests/testSolvers.py
@@ -788,18 +788,6 @@ class TestSolvers(unittest.TestCase):
         matrix1 = rand(self.matrix_dimension, self.matrix_dimension,
                        density=1.0, random_state=1)
         matrix1 = csr_matrix(matrix1 + matrix1.T)
-        # matrix1 = csr_matrix([[11, 12, 13, 14, 15, 16, 17, 18],
-        #                       [21, 22, 23, 24, 25, 26, 27, 28],
-        #                       [31, 32, 33, 34, 35, 36, 37, 38],
-        #                       [41, 42, 43, 44, 45, 46, 47, 48],
-        #                       [51, 52, 53, 54, 55, 56, 57, 58],
-        #                       [61, 62, 63, 64, 65, 66, 67, 68],
-        #                       [71, 72, 73, 74, 75, 76, 77, 78],
-        #                       [81, 82, 83, 84, 85, 86, 87, 88]])
-        # matrix1 = csr_matrix([[11, 12, 13, 14],
-        #                       [21, 22, 23, 24],
-        #                       [31, 32, 33, 34],
-        #                       [41, 42, 43, 44]])
         if (self.my_rank == 0):
             mmwrite(self.input_file, matrix1)
         w, vdense = eigh(matrix1.todense())
