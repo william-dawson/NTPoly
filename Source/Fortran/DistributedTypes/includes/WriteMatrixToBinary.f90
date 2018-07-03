3c77f04a4840a41d2c80728400d12c97e00f89b2
William Dawson <william.dawson@riken.jp>
2018-07-03 11:38:03 +0900

    Forgot the new files

---
+++ Source/Fortran/DistributedTypes/includes/WriteMatrixToBinary.f90
@@ -0,0 +1,69 @@
+  !! Local Data
+  INTEGER, DIMENSION(:), ALLOCATABLE :: local_values_buffer
+  INTEGER :: mpi_file_handler
+  INTEGER(KIND=MPI_OFFSET_KIND) :: header_size
+  INTEGER(KIND=MPI_OFFSET_KIND) :: write_offset
+  !! Temporary Variables
+  INTEGER :: temp_int
+  INTEGER :: bytes_per_int, bytes_per_data
+  INTEGER, DIMENSION(4) :: header_buffer
+  INTEGER :: mpi_status(MPI_STATUS_SIZE)
+  INTEGER(KIND=MPI_OFFSET_KIND) :: zero_offset = 0
+  INTEGER :: counter
+  INTEGER :: ierr
+
+  !! Merge all the local data
+  CALL MergeMatrixLocalBlocks(this, merged_local_data)
+
+  !! Determine Write Location
+  bytes_per_int = sizeof(temp_int)
+  bytes_per_data = sizeof(temp_data)
+  header_size = bytes_per_int*4
+  ALLOCATE(local_values_buffer(this%process_grid%slice_size))
+  CALL MPI_Allgather(SIZE(merged_local_data%values),1,MPI_INT,&
+       & local_values_buffer,1,MPI_INT,&
+       & this%process_grid%within_slice_comm,ierr)
+  write_offset = 0
+  write_offset = write_offset + header_size
+  DO counter = 1,this%process_grid%within_slice_rank
+     write_offset = write_offset + &
+          & local_values_buffer(counter)*(bytes_per_int*2+bytes_per_data*1)
+  END DO
+
+  !! Write The File
+  IF (this%process_grid%between_slice_rank .EQ. 0) THEN
+     !! Create Special MPI Type
+     CALL MatrixToTripletList(merged_local_data, triplet_list)
+     !! Absolute Positions
+     CALL ShiftTripletList(triplet_list, this%start_row - 1, &
+          & this%start_column - 1)
+     CALL MPI_File_open(this%process_grid%within_slice_comm,file_name,&
+          & IOR(MPI_MODE_CREATE,MPI_MODE_WRONLY),MPI_INFO_NULL, &
+          & mpi_file_handler, ierr)
+     !! Write Header
+     IF (this%process_grid%within_slice_rank .EQ. 0) THEN
+        header_buffer(1) = this%actual_matrix_dimension
+        header_buffer(2) = this%actual_matrix_dimension
+        header_buffer(3) = SUM(local_values_buffer)
+        IF (this%is_complex) THEN
+           header_buffer(4) = 1
+        ELSE
+           header_buffer(4) = 0
+        END IF
+        CALL MPI_File_write_at(mpi_file_handler,zero_offset,header_buffer,4,&
+             & MPI_INT,mpi_status,ierr)
+     END IF
+     !! Write The Rest
+     CALL MPI_File_set_view(mpi_file_handler,write_offset,triplet_mpi_type,&
+          & triplet_mpi_type,"native",MPI_INFO_NULL,ierr)
+     CALL MPI_File_write(mpi_file_handler,triplet_list%data, &
+          & triplet_list%CurrentSize, triplet_mpi_type, MPI_STATUS_IGNORE, &
+          & ierr)
+
+     !! Cleanup
+     CALL MPI_File_close(mpi_file_handler,ierr)
+     CALL DestructTripletList(triplet_list)
+  END IF
+  DEALLOCATE(local_values_buffer)
+  CALL MPI_Barrier(this%process_grid%global_comm,ierr)
+  CALL DestructMatrix(merged_local_data)
---
+++ Source/Fortran/DistributedTypes/includes/WriteToMatrixMarket.f90
@@ -0,0 +1,132 @@
+  !! Parameters
+  INTEGER, PARAMETER :: MAX_LINE_LENGTH = 1024
+  !! Local MPI Variables
+  INTEGER :: mpi_file_handler
+  INTEGER :: mpi_status(MPI_STATUS_SIZE)
+  INTEGER, DIMENSION(:), ALLOCATABLE :: local_values_buffer
+  !! Local Data
+  INTEGER :: triplet_list_string_length
+  INTEGER(KIND=MPI_OFFSET_KIND) :: header_size
+  INTEGER(KIND=MPI_OFFSET_KIND) :: write_offset
+  INTEGER(KIND=MPI_OFFSET_KIND) :: header_offset
+  INTEGER(KIND=MPI_OFFSET_KIND), PARAMETER :: zero_size = 0
+  !! Strings
+  CHARACTER(len=:), ALLOCATABLE :: header_line1
+  CHARACTER(len=:), ALLOCATABLE :: header_line2
+  CHARACTER(len=:), ALLOCATABLE :: write_buffer
+  !! Temporary Values
+  INTEGER :: counter
+  INTEGER :: offset_counter
+  INTEGER :: NEW_LINE_LENGTH
+  CHARACTER(len=MAX_LINE_LENGTH*2) :: temp_string1
+  CHARACTER(len=MAX_LINE_LENGTH) :: temp_string2
+  INTEGER :: temp_length
+  INTEGER :: bytes_per_character
+  CHARACTER(len=1) :: temp_char
+  INTEGER :: ierr
+
+  !! Merge all the local data
+  CALL MergeMatrixLocalBlocks(this, merged_local_data)
+
+  bytes_per_character = sizeof(temp_char)
+
+  !! Create the matrix size line
+  NEW_LINE_LENGTH = LEN(new_line('A'))
+#ifdef ISCOMPLEX
+  WRITE(temp_string1,'(A)') "%%MatrixMarket matrix coordinate complex general" &
+       & //new_line('A')//"%"//new_line('A')
+#else
+  WRITE(temp_string1,'(A)') "%%MatrixMarket matrix coordinate real general" &
+       & //new_line('A')//"%"//new_line('A')
+#endif
+  ALLOCATE(CHARACTER(&
+       & len=LEN_TRIM(temp_string1)) :: header_line1)
+  header_line1 = TRIM(temp_string1)
+
+  WRITE(temp_string2,*) this%actual_matrix_dimension, &
+       & this%actual_matrix_dimension, GetMatrixSize(this)
+  !! I don't understand why the +1 is needed, but it is.
+  ALLOCATE(CHARACTER(&
+       & len=LEN_TRIM(temp_string2)+NEW_LINE_LENGTH+1) :: header_line2)
+  WRITE(header_line2,*) TRIM(temp_string2)//new_line('A')
+
+  header_size = LEN(header_line1) + LEN(header_line2)
+
+  !! Local Data
+  CALL MatrixToTripletList(merged_local_data, triplet_list)
+
+  !! Absolute Positions
+  CALL ShiftTripletList(triplet_list, this%start_row - 1, &
+       & this%start_column - 1)
+
+  !! Figure out the length of the string for storing.
+  triplet_list_string_length = 0
+  DO counter = 1, triplet_list%CurrentSize
+#ifdef ISCOMPLEX
+     WRITE(temp_string2,*) triplet_list%data(counter)%index_row, &
+          & triplet_list%data(counter)%index_column, &
+          & REAL(triplet_list%data(counter)%point_value), &
+          & AIMAG(triplet_list%data(counter)%point_value), &
+          & new_line('A')
+#else
+     WRITE(temp_string2,*) triplet_list%data(counter)%index_row, &
+          & triplet_list%data(counter)%index_column, &
+          & triplet_list%data(counter)%point_value, &
+          & new_line('A')
+#endif
+     triplet_list_string_length = triplet_list_string_length + &
+          & LEN_TRIM(temp_string2)
+     triplet_list_string_length = triplet_list_string_length + NEW_LINE_LENGTH
+  END DO
+
+  !! Write that string to the write buffer
+  ALLOCATE(CHARACTER(len=triplet_list_string_length+1) :: write_buffer)
+  offset_counter = 1
+  DO counter = 1, triplet_list%CurrentSize
+     WRITE(temp_string2,*) triplet_list%data(counter)%index_row, &
+          & triplet_list%data(counter)%index_column, &
+          & triplet_list%data(counter)%point_value, &
+          & new_line('A')
+     temp_length = LEN_TRIM(temp_string2)+NEW_LINE_LENGTH
+     WRITE(write_buffer(offset_counter:offset_counter+temp_length),*) &
+          & temp_string2(1:temp_length)
+     offset_counter = offset_counter + temp_length
+  END DO
+
+  !! Figure out the offset sizes
+  ALLOCATE(local_values_buffer(this%process_grid%slice_size))
+  CALL MPI_Allgather(triplet_list_string_length,1,MPI_INT,&
+       & local_values_buffer,1,MPI_INT,this%process_grid%within_slice_comm,ierr)
+  write_offset = 0
+  write_offset = write_offset + header_size
+  DO counter = 1,this%process_grid%within_slice_rank
+     write_offset = write_offset + &
+          & local_values_buffer(counter)
+  END DO
+
+  !! Global Write
+  IF (this%process_grid%between_slice_rank .EQ. 0) THEN
+     CALL MPI_File_open(this%process_grid%within_slice_comm,file_name, &
+          & IOR(MPI_MODE_CREATE,MPI_MODE_WRONLY),MPI_INFO_NULL, &
+          & mpi_file_handler,ierr)
+     CALL MPI_File_set_size(mpi_file_handler,zero_size,ierr)
+     !! Write Header
+     IF (this%process_grid%within_slice_rank .EQ. 0) THEN
+        header_offset = 0
+        CALL MPI_File_write_at(mpi_file_handler,header_offset,header_line1, &
+             & LEN(header_line1), MPI_CHARACTER,mpi_status,ierr)
+        header_offset = header_offset + LEN(header_line1)
+        CALL MPI_File_write_at(mpi_file_handler,header_offset,header_line2, &
+             & LEN(header_line2), MPI_CHARACTER,mpi_status,ierr)
+     END IF
+     !! Write Local Data
+     CALL MPI_File_set_view(mpi_file_handler,write_offset,MPI_CHARACTER,&
+          & MPI_CHARACTER,"native",MPI_INFO_NULL,ierr)
+     CALL MPI_File_write(mpi_file_handler,write_buffer, &
+          & triplet_list_string_length,&
+          & MPI_CHARACTER,MPI_STATUS_IGNORE,ierr)
+
+     !! Cleanup
+     CALL MPI_File_close(mpi_file_handler,ierr)
+  END IF
+  CALL MPI_Barrier(this%process_grid%global_comm,ierr)
