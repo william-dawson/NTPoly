19f86469d36ce40b027df5bb7ee8d0fad6afa9f2
William Dawson <william.dawson@riken.jp>
2018-09-25 19:33:11 +0900

    Small optimization

--- Source/Fortran/distributed_includes/FillMatrixFromTripletList.f90
+++ Source/Fortran/distributed_includes/FillMatrixFromTripletList.f90
@@ -18,7 +18,8 @@
        & this%local_rows, this%local_columns)
   !! And reduce over the Z dimension. This can be accomplished by
   !! summing up.
-  IF (.NOT. preduplicated) THEN
+  IF (.NOT. preduplicated .AND. &
+       & .NOT. this%process_grid%num_process_slices .EQ. 1) THEN
      CALL ReduceMatrixSizes(local_matrix, this%process_grid%between_slice_comm,&
           & gather_helper)
      DO WHILE(.NOT. TestReduceSizeRequest(gather_helper))
