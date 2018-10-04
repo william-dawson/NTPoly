7ed593ea93b968df73bce4b00e12e22a234329a6
William Dawson <dawson@fairport.r-ccs27.riken.jp>
2018-10-04 19:10:25 +0900

    Enriched the process grid interface

--- Source/C/ProcessGrid_c.h
+++ Source/C/ProcessGrid_c.h
@@ -6,20 +6,30 @@ void ConstructGlobalProcessGrid_wrp(const int *world_comm,
                                     const int *process_columns,
                                     const int *process_slices,
                                     const bool *be_verbose);
-void CopyProcessGrid_wrp(const int *ih_old_grid, int* ih_new_grid);
+void ConstructGlobalProcessGridabr_wrp(const int *world_comm,
+                                       const int *process_slices,
+                                       const bool *be_verbose);
+void CopyProcessGrid_wrp(const int *ih_old_grid, int *ih_new_grid);
 int GetGlobalMySlice_wrp();
 int GetGlobalMyColumn_wrp();
 int GetGlobalMyRow_wrp();
+int GetGlobalNumSlices_wrp();
+int GetGlobalNumColumns_wrp();
+int GetGlobalNumRows_wrp();
 void DestructGlobalProcessGrid_wrp();
 
 void ConstructProcessGrid_wrp(int *ih_grid, const int *world_comm,
                               const int *process_rows,
                               const int *process_columns,
-                              const int *process_slices,
-                              const bool *be_verbose);
+                              const int *process_slices);
+void ConstructProcessGridabr_wrp(int *ih_grid, const int *world_comm,
+                                 const int *process_slices);
 int GetMySlice_wrp(const int *ih_grid);
 int GetMyColumn_wrp(const int *ih_grid);
 int GetMyRow_wrp(const int *ih_grid);
+int GetNumSlices_wrp(const int *ih_grid);
+int GetNumColumns_wrp(const int *ih_grid);
+int GetNumRows_wrp(const int *ih_grid);
 void DestructProcessGrid_wrp(int *ih_grid);
 
 #endif
