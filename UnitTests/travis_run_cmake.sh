76419e12a714b96debad788f41ffc448a2e381c6
William Dawson <dawson@fairport.r-ccs27.riken.jp>
2018-11-09 16:39:16 +0900

    Travis check for modifications.

--- UnitTests/travis_run_cmake.sh
+++ UnitTests/travis_run_cmake.sh
@@ -1,15 +1,14 @@
 if [[ "$TESTOS" == "OSX" ]]; then
-  cmake -DCMAKE_TOOLCHAIN_FILE=../Targets/Mac-python2.cmake \
-    -DCMAKE_BUILD_TYPE=Release .. ;
+  cmake .. -DCMAKE_TOOLCHAIN_FILE=../Targets/Mac-python2.cmake \
+           -DCMAKE_BUILD_TYPE=Release ;
 else
   if  [ ! -z ${NOIALLGATHER+x} ]; then
-    cmake -DCMAKE_TOOLCHAIN_FILE=../Targets/Linux.cmake \
-          -DNOIALLGATHER=YES .. -DCMAKE_BUILD_TYPE=Release ;
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DNOIALLGATHER=YES ;
   elif [ -z ${FORTRAN_ONLY+x} ]; then
-    cmake -DCMAKE_TOOLCHAIN_FILE=../Targets/Linux.cmake .. \
-          -DCMAKE_BUILD_TYPE=Release ;
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=../Targets/Linux.cmake \
+             -DCMAKE_BUILD_TYPE=Release ;
   else
-    cmake -DCMAKE_TOOLCHAIN_FILE=../Targets/Linux.cmake \
-          -DFORTRAN_ONLY=YES .. -DCMAKE_BUILD_TYPE=Release ;
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=../Targets/Linux.cmake \
+             -DFORTRAN_ONLY=YES -DCMAKE_BUILD_TYPE=Release ;
   fi
 fi
