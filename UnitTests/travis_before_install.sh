514bee83da4b37d5edb6e064bfcf3bb22053ed6e
William Dawson <dawson@fairport.r-ccs27.riken.jp>
2018-11-12 16:24:48 +0900

    Try older version of mpi?

--- UnitTests/travis_before_install.sh
+++ UnitTests/travis_before_install.sh
@@ -2,16 +2,16 @@ if [[ "$TESTOS" == "LINUX" ]]; then
   sudo apt-get update
   sudo apt-get install gfortran
   sudo apt-get install libblas-dev liblapack-dev
-  if [ -f "openmpi-3.1.3/README" ]; then
+  if [ -f "openmpi-3.0.1/README" ]; then
     echo "Using cached openmpi";
   else
-    wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.3.tar.gz;
-    tar xvf openmpi-3.1.3.tar.gz >/dev/null;
-    cd openmpi-3.1.3;
+    wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.0.1.tar.gz;
+    tar xvf openmpi-3.0.1.tar.gz >/dev/null;
+    cd openmpi-3.0.1;
     ./configure >/dev/null 2>&1;
     make >/dev/null 2>&1;
   fi
-  cd openmpi-3.1.3
+  cd openmpi-3.0.1
   sudo make install >/dev/null 2>&1
   cd ../
 fi
