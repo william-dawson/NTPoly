70c42c9708c0f25cf7e3a88095272b93301a925f
william-dawson <william.d.f.dawson@gmail.com>
2018-11-20 16:41:39 +0900

    Code reduction for gathering a matrix to a single process (#87)

--- Source/Fortran/distributed_includes/PrintMatrix.f90
+++ Source/Fortran/distributed_includes/PrintMatrix.f90
@@ -1,53 +1,11 @@
-  !! Helpers For Communication
-  TYPE(ReduceHelper_t) :: row_helper
-  TYPE(ReduceHelper_t) :: column_helper
-
-  !! Merge all the local data
-  CALL MergeMatrixLocalBlocks(this, merged_local_data)
-
-  !! Merge Columns
-  CALL TransposeMatrix(merged_local_data, merged_local_dataT)
-  CALL ReduceAndComposeMatrixSizes(merged_local_dataT, &
-       & this%process_grid%column_comm, merged_columns, column_helper)
-  DO WHILE(.NOT. TestReduceSizeRequest(column_helper))
-  END DO
-  CALL ReduceAndComposeMatrixData(merged_local_dataT, &
-       & this%process_grid%column_comm, merged_columns, &
-       & column_helper)
-  DO WHILE(.NOT. TestReduceInnerRequest(column_helper))
-  END DO
-  DO WHILE(.NOT. TestReduceDataRequest(column_helper))
-  END DO
-  CALL ReduceAndComposeMatrixCleanup(merged_local_dataT, merged_columns, &
-       & column_helper)
-
-  !! Merge Rows
-  CALL TransposeMatrix(merged_columns,merged_columnsT)
-  CALL ReduceAndComposeMatrixSizes(merged_columnsT, &
-       & this%process_grid%row_comm, full_gathered, row_helper)
-  DO WHILE(.NOT. TestReduceSizeRequest(row_helper))
-  END DO
-  CALL ReduceAndComposeMatrixData(merged_columnsT, this%process_grid%row_comm, &
-       & full_gathered, row_helper)
-  DO WHILE(.NOT. TestReduceInnerRequest(row_helper))
-  END DO
-  DO WHILE(.NOT. TestReduceDataRequest(row_helper))
-  END DO
-  CALL ReduceAndComposeMatrixCleanup(merged_columnsT, full_gathered,row_helper)
-
-  !! Make these changes so that it prints the logical rows/columns
-  full_gathered%rows = this%actual_matrix_dimension
-  full_gathered%columns = this%actual_matrix_dimension
+  CALL GatherMatrixToProcess(this, local_mat, 0)
 
   IF (IsRoot(this%process_grid)) THEN
      IF (PRESENT(file_name_in)) THEN
-        CALL PrintMatrix(full_gathered, file_name_in)
+        CALL PrintMatrix(local_mat, file_name_in)
      ELSE
-        CALL PrintMatrix(full_gathered)
+        CALL PrintMatrix(local_mat)
      END IF
   END IF
 
-  CALL DestructMatrix(merged_local_data)
-  CALL DestructMatrix(merged_local_dataT)
-  CALL DestructMatrix(merged_columns)
-  CALL DestructMatrix(merged_columnsT)
+  CALL DestructMatrix(local_mat)
