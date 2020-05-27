bdc2ff505b21711a1dcbd151e333545268a31f68
William Dawson <william.dawson@riken.jp>
2020-05-27 17:29:33 +0900

    More narrowing

--- UnitTests/lint.sh
+++ UnitTests/lint.sh
@@ -1,3 +1,8 @@
+which flake8
+which clang-format
+which emacs
+which awk
+
 # Python
 flake8 UnitTests
 flake8 Examples
@@ -16,6 +21,7 @@ for f in $(find . -type f -name "*.*90"); do
                   --eval="(f90-indent-subprogram)" -f save-buffer 2>/dev/null
 done
 
+set -e
 # 80 Column Limit
 for f in $(find -L Source Examples Targets UnitTests); do
 	awk 'NF > 80 {print FILENAME ; print "Line " NR ; print ; stat = 1} \
