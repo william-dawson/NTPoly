825c31ee92b4585d7ecb868891bc0edabae7a70a
William Dawson <william.dawson@riken.jp>
2020-05-27 14:29:48 +0900

    More linting work

--- UnitTests/lint.sh
+++ UnitTests/lint.sh
@@ -1,7 +1,15 @@
+set -e
+
+# Python
 flake8 UnitTests
 flake8 Examples
+
+# C/C++
 clang-format -style=llvm Source/C/*.h -i
 clang-format -style=llvm Source/CPlusPlus/*.h -i
 clang-format -style=llvm Source/CPlusPlus/*.cc -i
 clang-format -style=llvm Examples/*/*.cc -i
-git --no-pager diff
+
+# If git returns any changes, we know there is a problem.
+git --no-pager diff --exit-code
+
