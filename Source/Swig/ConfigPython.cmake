67d28ef1f8cab1513d08d7acc38b6925cbafac50
william-dawson <william.dawson@riken.jp>
2020-11-16 11:48:53 +0900

    Simplified configuration on a Mac (#161)

--- Source/Swig/ConfigPython.cmake
+++ Source/Swig/ConfigPython.cmake
@@ -6,15 +6,3 @@ macro(get_py_include)
       "from distutils.sysconfig import get_python_inc; print(get_python_inc())"
       OUTPUT_VARIABLE PYTHON_INCLUDE_PATH OUTPUT_STRIP_TRAILING_WHITESPACE)
 endmacro()
-
-################################################################################
-# Determine Python Library Path
-macro(get_py_lib)
-  execute_process(
-      COMMAND ${PYTHON_EXECUTABLE} -c
-"
-from distutils.sysconfig import get_python_lib
-path=get_python_lib(standard_lib=True)+\"/../../Python\"
-print(path)"
-    OUTPUT_VARIABLE PYTHON_LIBRARIES OUTPUT_STRIP_TRAILING_WHITESPACE)
-endmacro()
