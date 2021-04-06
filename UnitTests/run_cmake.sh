b8c538e6cf0c1fb734ee16c018c9f469a0c7ad32
William Dawson <william.dawson@riken.jp>
2021-04-06 14:30:46 +0900

    Caps

--- UnitTests/run_cmake.sh
+++ UnitTests/run_cmake.sh
@@ -1,15 +1,15 @@
 if [[ "$TESTOS" == "OSX" ]]; then
-  cmake -G ninja .. -DCMAKE_TOOLCHAIN_FILE=../Targets/Mac-python3.cmake \
+  cmake -G Ninja .. -DCMAKE_TOOLCHAIN_FILE=../Targets/Mac-python3.cmake \
     -DCMAKE_BUILD_TYPE=Release ;
 else
   if  [ ! -z ${NOIALLGATHER+x} ]; then
-    cmake -G ninja .. \
+    cmake -G Ninja .. \
           -DCMAKE_BUILD_TYPE=Debug -DNOIALLGATHER=YES;
   elif [ ! -z ${DEBUG+x} ]; then
-    cmake -G ninja .. -DCMAKE_TOOLCHAIN_FILE=../Targets/Linux.cmake \
+    cmake -G Ninja .. -DCMAKE_TOOLCHAIN_FILE=../Targets/Linux.cmake \
           -DCMAKE_BUILD_TYPE=Debug ;
   else
-    cmake -G ninja .. -DCMAKE_TOOLCHAIN_FILE=../Targets/Linux.cmake \
+    cmake -G Ninja .. -DCMAKE_TOOLCHAIN_FILE=../Targets/Linux.cmake \
           -DCMAKE_BUILD_TYPE=Release ;
   fi
 fi
