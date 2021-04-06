ba7d80dbc77e9405e082334cc0496cb28e39f14f
William Dawson <william.dawson@riken.jp>
2021-04-06 13:37:33 +0900

    Python3 to cmake

--- UnitTests/run_cmake.sh
+++ UnitTests/run_cmake.sh
@@ -1,6 +1,6 @@
 if [[ "$TESTOS" == "OSX" ]]; then
   cmake .. -DCMAKE_TOOLCHAIN_FILE=../Targets/Mac-python2.cmake \
-    -DCMAKE_BUILD_TYPE=Release ;
+    -DCMAKE_BUILD_TYPE=Release -DPythonInterp=python3 ;
 else
   if  [ ! -z ${NOIALLGATHER+x} ]; then
     cmake .. \
