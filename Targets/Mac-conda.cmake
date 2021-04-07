ac3f584289e19837744a32c6021d3f0052ca45e8
william-dawson <william.dawson@riken.jp>
2021-04-07 19:25:05 +0900

    Use Anaconda in the CI Stage (#167)
    * Conda support
    
    * Fix test list
    
    * Fix activation
    
    * Try the marketplace
    
    * Have to restrict conda to just one build
    
    * Try separting the scripts
    
    * Typo
    
    * Another typo fix!
    
    * Need to explicitly set fail condition
    
    * Fix env variable
    
    * Try another market
    
    * Typo
    
    * Sourcing has worked in the past...
    
    * Source more explicitly
    
    * Another idea
    
    * Try using base
    
    * Defaults
    
    * -l
    
    * Typo
    
    * Almost working...
    
    * All conda
    
    * Restore line
    
    * Order fix
    
    * Even simpler
    
    * Version?
    
    * modify command
    
    * Try installing bash?
    
    * Another idea
    
    * Auto activate problem
    
    * Has to be conda
    
    * Debug by listing
    
    * Switch mac back to standard python
    
    * Further cleanup
    
    * Mac needs brew

---
+++ Targets/Mac-conda.cmake
@@ -0,0 +1,21 @@
+################################################################################
+# Target file for a Mac computer using anaconda. The important ingredient
+# is modifying the find strategy.
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_C_COMPILER mpicc)
+set(CMAKE_Fortran_COMPILER mpif90)
+set(CMAKE_CXX_COMPILER mpicxx)
+set(PYTHON_EXECUTABLE python)
+set(Python_FIND_STRATEGY LOCATION)
+
+# Library Files
+set(TOOLCHAIN_LIBS "-framework Accelerate -lgomp")
+
+# Release Suggestions
+set(CXX_TOOLCHAINFLAGS_RELEASE "-O3 -openmp")
+set(F_TOOLCHAINFLAGS_RELEASE "-O3 -cpp -fopenmp -Wall")
+
+# Debug suggestions
+set(CXX_TOOLCHAINFLAGS_DEBUG "-O0")
+set(F_TOOLCHAINFLAGS_DEBUG
+  "-fcheck=all -O0 -fexternal-blas -cpp -Wall -DPURE=")
