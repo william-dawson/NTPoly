8abef4c09942fc71e588954f3c8711a13b0c9298
William Dawson <william.dawson@riken.jp>
2021-08-30 14:02:23 +0900

    Try CI for thread cap

--- UnitTests/run_ci_test.sh
+++ UnitTests/run_ci_test.sh
@@ -4,6 +4,11 @@ if [[ "$TESTOS" == "LINUX" ]]; then
    conda activate ntpoly-conda
 fi
 
+if [[ "$threadoff" == "1" ]]; then
+   export OMP_NUM_THREADS=1
+   echo "Setting threads to 1"
+fi
+
 cd Build
 export CTEST_OUTPUT_ON_FAILURE=1
 eval "$MAKETEST"
