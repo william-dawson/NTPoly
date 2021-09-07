a70855aa9a2d6a71d4938fb1de7d48309b353bc3
William Dawson <william.dawson@riken.jp>
2021-09-07 22:46:07 +0900

    Added a lint phase

--- UnitTests/generate_elsi.py
+++ UnitTests/generate_elsi.py
@@ -3,9 +3,10 @@ Generate the source code to be included in ELSI.
 
 The steps are:
     1) Run CMake with the -S -save-temps options.
-    2) Make the Fortran library
+    2) Make the Fortran library.
     3) Copy the preprocessed files into an exportable directory.
     4) Place the custom Header switch files.
+    5) Lint.
 """
 from os import mkdir, getcwd, chdir, system
 from os.path import join, basename
@@ -74,3 +75,10 @@ if __name__ == "__main__":
     with open(join("ElsiBuild", "elsi_output",
                    "NTMPIFH.f90"), "w") as ofile:
         ofile.write(switch2)
+
+    # Might as well lint the result as well.
+    finished_list = glob(join("ElsiBuild", "elsi_output", "*.f90"))
+    args = '''--eval="(f90-mode)" --eval="(f90-indent-subprogram)"''' + \
+           ''' -f save-buffer'''
+    for f in finished_list:
+        system("emacs -batch " + f + " " + args)
