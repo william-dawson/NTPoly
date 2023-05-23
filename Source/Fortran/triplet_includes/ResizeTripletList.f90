b1530c95df682d3de39878ef990e2d20d24855cf
William Dawson <william.dawson@riken.jp>
2023-05-23 19:14:55 +0900

    Bug fix

--- Source/Fortran/triplet_includes/ResizeTripletList.f90
+++ Source/Fortran/triplet_includes/ResizeTripletList.f90
@@ -1,13 +1,20 @@
+  INTEGER :: old_size
+
   !! Temporary copy
-  ALLOCATE(temporary_data(this%CurrentSize))
-  temporary_data(:) = this%DATA(:this%CurrentSize)
+  old_size = this%CurrentSize
+  ALLOCATE(temporary_data(old_size))
+  temporary_data(:) = this%DATA(:old_size)
 
   !! Create new memory
   IF (ALLOCATED(this%DATA)) DEALLOCATE(this%DATA)
   ALLOCATE(this%DATA(size))
 
   !! Copy back
-  this%DATA(:) = temporary_data
+  IF (old_size .LT. size) THEN
+     this%DATA(:old_size) = temporary_data(:old_size)
+  ELSE
+     this%DATA(:size) = temporary_data(:size)
+  END IF
 
   !! Cleanup
   DEALLOCATE(temporary_data)
