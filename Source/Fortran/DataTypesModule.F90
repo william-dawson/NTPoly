9a8a1aecd7ed4897791fa86a9bec74e7b267db9b
william-dawson <william.dawson@riken.jp>
2023-07-05 16:03:42 +0900

    Use a suitable Fortran type for MPI version of long integer (#208)

--- Source/Fortran/DataTypesModule.F90
+++ Source/Fortran/DataTypesModule.F90
@@ -19,6 +19,6 @@ MODULE DataTypesModule
   !> MPI Integer type we will use in this program.
   INTEGER, PARAMETER, PUBLIC :: MPINTINTEGER = MPI_INTEGER
   !> MPI Integer type we will use in this program.
-  INTEGER, PARAMETER, PUBLIC :: MPINTLONG = MPI_LONG
+  INTEGER, PARAMETER, PUBLIC :: MPINTLONG = MPI_INTEGER8
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 END MODULE DataTypesModule
