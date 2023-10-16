c9d018c73e43ea169b1b5cfaf1987d90f14ec79d
william-dawson <william.dawson@riken.jp>
2023-10-16 16:06:07 +0900

    Fix matrix binary ops for large case where int4 isn't enough (#222)
    * Fix matrix binary ops for large case where int4 isn't enough
    
    * fix offset too
    
    * lint

--- Source/Fortran/PSMatrixModule.F90
+++ Source/Fortran/PSMatrixModule.F90
@@ -651,11 +651,12 @@ CONTAINS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
        !! Compute Offset
        local_triplets = total_values / this%process_grid%total_processors
-       local_offset = local_triplets * this%process_grid%global_rank
+       local_offset = INT(local_triplets, KIND=NTLONG) * &
+            & this%process_grid%global_rank
        header_size = 3 * bytes_per_int + bytes_per_long
        IF (this%process_grid%global_rank .EQ. &
             & this%process_grid%total_processors - 1) THEN
-          local_triplets = INT(total_values) - INT(local_offset)
+          local_triplets = INT(total_values - local_offset)
        END IF
        local_offset = local_offset*(bytes_per_int*2 + bytes_per_data) + &
             & header_size
