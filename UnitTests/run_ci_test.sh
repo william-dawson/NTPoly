316798f5ad0324b119b947e59aee3f35575754aa
William Dawson <william.dawson@riken.jp>
2023-11-15 16:27:23 +0900

    Simplify thread setting

--- UnitTests/run_ci_test.sh
+++ UnitTests/run_ci_test.sh
@@ -4,13 +4,7 @@ if [[ "$TESTOS" == "LINUX" ]]; then
    conda activate ntpoly-conda
 fi
 
-if [[ "$THREADOFF" == "1" ]]; then
-   export OMP_NUM_THREADS=1
-   echo "Setting threads to 1"
-else
-   export OMP_NUM_THREADS=2
-   echo "Setting threads to 2"
-fi
+echo "Setting threads to $OMP_NUM_THREADS"
 
 cd Build
 export CTEST_OUTPUT_ON_FAILURE=1
