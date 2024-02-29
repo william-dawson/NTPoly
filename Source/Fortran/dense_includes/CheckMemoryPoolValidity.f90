95f656689d44581086bb149393357f6358a2d172
William Dawson <william.dawson@riken.jp>
2024-02-29 18:26:01 +0900

    Backport fixes (#188) to 2.7.1

--- Source/Fortran/dense_includes/CheckMemoryPoolValidity.f90
+++ Source/Fortran/dense_includes/CheckMemoryPoolValidity.f90
@@ -3,10 +3,12 @@
   IF (.NOT. ALLOCATED(this%pruned_list)) isvalid = .FALSE.
   IF (.NOT. ALLOCATED(this%value_array)) isvalid = .FALSE.
 
-  !! Check allocation size
-  IF (.NOT. SIZE(this%value_array,dim=2) .EQ. rows) THEN
-     isvalid = .FALSE.
-  END IF
-  IF (.NOT. SIZE(this%value_array,dim=1) .EQ. columns) THEN
-     isvalid = .FALSE.
+  IF (isvalid) THEN
+     !! Check allocation size
+     IF (.NOT. SIZE(this%value_array,dim=2) .EQ. rows) THEN
+        isvalid = .FALSE.
+     END IF
+     IF (.NOT. SIZE(this%value_array,dim=1) .EQ. columns) THEN
+        isvalid = .FALSE.
+     END IF
   END IF
